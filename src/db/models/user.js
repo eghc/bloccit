9761adeacfb581d28d3bd9896ed3e2d4446c78cc
eghc <eringraceharris@gmail.com>
2019-03-24 08:06:37 -0700

    voting checkpoint completed

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -27,6 +27,11 @@ module.exports = (sequelize, DataTypes) => {
       foreignKey: "userId",
       as: "comments"
     });
+
+    User.hasMany(models.Vote, {
+      foreignKey: "userId",
+      as: "votes"
+    });
   };
   User.prototype.isAdmin = function() {
     return this.role === "admin";
